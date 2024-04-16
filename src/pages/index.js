0df22135fa7d698e379584be996dd00644a92b6b
vuquyet808 <vuquyet808@gmail.com>
2024-04-16 23:09:41 +0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,29 @@
 import { Inter } from "next/font/google";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const telegramWrapperRef = useRef(null);
+  useEffect(() => {
+    const scriptElement = document.createElement("script");
+    scriptElement.src = "https://telegram.org/js/telegram-widget.js?22";
+    scriptElement.setAttribute("data-telegram-login", "Tsnake_fun_bot");
+    scriptElement.setAttribute("data-size", "large");
+    scriptElement.setAttribute("data-auth-url", "/test");
+    scriptElement.async = true;
+
+    telegramWrapperRef.current.appendChild(scriptElement);
+  }, []);
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <div className="w-20 h-20 bg-red-600">
+        <div ref={telegramWrapperRef}></div>
+      </div>
+
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
